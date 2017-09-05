ba103cde2e857dcef130ffe0d5d4a9c97565ef1e
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2017-09-05 17:13:00 -0400

    Adding Agent code

--- project1/lib/project1.ex
+++ project1/lib/project1.ex
@@ -1,48 +1,53 @@
-defmodule Project1 do
 
-  # use Mix.Task
-  @moduledoc """
-  Documentation for Project1.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Project1.hello
-      :world
-
-  """
-  def get_matching_String(num, s) do
-
-    if(hash_code_match(s,1,num)) do
-      IO.puts "HashCode Matched"
-      IO.puts s <> <<num::utf8>>
-
-    else
-        get_matching_String(num+1, s)
-
-    end
-
-  end
-
-  def hash_code_match(s, zero_count, num) do
+defmodule Actor do
+  use GenServer
+    
+  def hash_code_match(check_str, zero_count) do
     zero_String = String.duplicate("0", zero_count)
-    check_str = s <> <<num::utf8>>
     a =  Base.encode16(:crypto.hash(:sha256, check_str))
     val = String.starts_with? a, zero_String
-    IO.puts "String is " <> check_str
-    IO.puts val
-    val
+    #IO.puts a
+    #IO.puts "String is " <> check_str
+    #IO.puts val
+    {val, check_str, a}
   end
 
-  def run() do
+  def gen_strings(str, k, cur, zero_count) when k == 0 do
+    hash_code_match(str, zero_count)
+  end
 
-    s = "amineni95:"
+  def gen_strings(str, k, cur, zero_count) when (cur > 125) do
+      {:false, str, ""}
+  end
 
-    get_matching_String(32, s)
-    
+  def gen_strings(str, k, cur, zero_count) do
+      {found, res, hash} = gen_strings(str <> <<cur::utf8>>, k-1, 33, zero_count)
+      if (found == :false) do
+        gen_strings(str, k, cur+1, zero_count)
+      else
+        {found, res, hash}
+      end
+  end
+  
+  def handle_cast({:process, client, str, k, zero_count}, state) do
+    {found, res, hash} = gen_strings(str, k, 33, zero_count)
+    if (found == :true) do
+      IO.puts found
+      IO.puts res
+      IO.puts hash
+      send(client, {found, res})
+      {:noreply, []}
+    else
+      handle_cast({:process, client, str, k-1, zero_count}, state)
+    end
+  end
+
+  def process(pid, {:process, client, str, k, zero_count}) do
+      GenServer.cast(pid, {:process, client, str, k, zero_count})
+  end
+  
+  def start_link do
+    GenServer.start_link(__MODULE__, [])
   end
 
 end

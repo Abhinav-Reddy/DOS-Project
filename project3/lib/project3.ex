27c477a84c7e1efc2ed062809e0b34f646cd4380
Abhinav-Reddy <abhinavpodduturi@ufl.edu>
2017-10-21 10:12:04 -0400

    Working route table construction

--- project3/lib/project3.ex
+++ project3/lib/project3.ex
@@ -1,8 +1,8 @@
 
 defmodule Actor do
   
-  def updateRow(destination, source, res) when destination == [] do
-    res
+  def updateRow(destination, _, res) when destination == [] do
+    Enum.reverse(res)
   end
 
   def updateRow(destination, source, res) do
@@ -14,14 +14,14 @@ defmodule Actor do
     updateRow(tl(destination), tl(source), [tmp | res])
   end
 
-  def updateRouteTable(routeTable, sourceRouteTable, nodeId, sourceKey, idx) 
+  def updateRouteTable(routeTable, _, _, _, idx) 
     when idx >= length(routeTable)-1 do
       routeTable
   end
 
   def updateRouteTable(routeTable, sourceRouteTable, nodeId, sourceKey, idx) do
     tmpRow = updateRow(Enum.at(routeTable, idx), Enum.at(sourceRouteTable, idx), [])
-    routeTable = List.insert_at(routeTable, idx, tmpRow)
+    routeTable = List.replace_at(routeTable, idx, tmpRow)
     if (String.at(nodeId, idx) != String.at(sourceKey, idx)) do
       routeTable
     else
@@ -29,39 +29,23 @@ defmodule Actor do
     end
   end
 
-  def getDistance(fromNode, toNode) do
-    if (toNode < fromNode) do
-      round(:math.pow(2,64)-1) - fromNode + toNode
-    else
-      toNode - fromNode
-    end
-  end
-
   def processLeafNodes(left, right, center, nodeId) do
     {_, pivot} = center
-    if (getDistance(nodeId, pivot) > getDistance(pivot, nodeId)) do
+    if (nodeId > pivot) do
       {left,right} = if (length(left) < 8) do
                       {left, right}
                      else
-                      [head | tail] = left
-                      if (length(right) < 8) do
-                        {tail, [right | head]}
-                      else
-                        {tail, right}
-                      end
+                      [_ | tail] = left
+                      {tail, right}
                      end
-      {[left | center], right}
+      {left ++ [center], right}
     else
       {left, right} = if (length(right) < 8) do
                         {left, right}
                       else
-                        [head | tail] = Enum.reverse(right)
+                        [_ | tail] = Enum.reverse(right)
                         tail = Enum.reverse(tail)
-                        if (length(left) < 8) do
-                          {[head | left], tail}
-                        else
-                          {left, tail}
-                        end
+                        {left, tail}
                       end
       {left, [center | right]}
     end
@@ -71,25 +55,30 @@ defmodule Actor do
   def receiveRouteInfo(routeTable, nodeId, receivedFrom) do
     receive do
       {:leafNodes, left, right, pivot} ->
-        {sourcePid, _} = pivot
+        {sourcePid, sourceKey} = pivot
         receivedFrom = [sourcePid | receivedFrom]
         {leftLeafs, rightLeafs} = processLeafNodes(left, right, pivot, nodeId)
+        #IO.puts "Received leaf nodes from " <> sourceKey
         {routeTable, leftLeafs, rightLeafs, receivedFrom}
+        
+
       {:routeTable, sourceRouteTable, sourcePid, sourceKey} ->
-        routeTable = updateRouteTable(routeTable, sourceRouteTable, to_string(nodeId), to_string(sourceKey), 0)
+        routeTable = updateRouteTable(routeTable, sourceRouteTable, nodeId, sourceKey, 0)
         receivedFrom = [ sourcePid | receivedFrom]
+        #IO.puts "Received route from " <> sourceKey
         receiveRouteInfo(routeTable, nodeId, receivedFrom)
     end
   end
   
-  def getNearestFromRemLeafs(leafs, key, diff, nearest) when leafs == [] do
+  def getNearestFromRemLeafs(leafs, _, _, nearest) when leafs == [] do
     nearest
   end
 
   def getNearestFromRemLeafs(leafs, key, diff, nearest) do
     {tmpId, tmpNodeId} = Enum.at(leafs, 0)
-    {diff, nearest} = if (diff < abs(key-tmpNodeId)) do
-                        {abs(key-tmpNodeId), pid}
+    tmpNodeId = String.to_integer(tmpNodeId, 16)
+    {diff, nearest} = if (diff > abs(key-tmpNodeId)) do
+                        {abs(key-tmpNodeId), tmpId}
                       else
                         {diff, nearest}
                       end
@@ -98,6 +87,7 @@ defmodule Actor do
 
   def getNearestFromLeafs(leafs, key) do
     {tmpId, tmpNodeId} = Enum.at(leafs, 0)
+    tmpNodeId = String.to_integer(tmpNodeId, 16)
     diff = abs(key - tmpNodeId)
     nearest = tmpId
     getNearestFromRemLeafs(tl(leafs), key, diff, nearest)
@@ -107,17 +97,17 @@ defmodule Actor do
     if (String.at(one, index) == String.at(two, index)) do
       compareIds(one, two, index+1)
     else
-      index+1
+      index
     end
   end
 
   def checkInRange(left, right, key) do
-    {minPid, minNodeId} = if (left != []) do
+    {_, minNodeId} = if (left != []) do
                             Enum.at(left, 0)
                           else
                             {:null, :null}
                           end
-    {maxPid, maxNodeId} = if (right != []) do
+    {_, maxNodeId} = if (right != []) do
                             Enum.at(right, -1)
                           else
                             {:null, :null}
@@ -127,80 +117,71 @@ defmodule Actor do
     end
 
     if (minNodeId <= key && key <= maxNodeId) do
-      getNearestFromLeafs([left | right], key)
+      getNearestFromLeafs(left ++ right, String.to_integer(key, 16))
     else
       :null
     end
   end
 
   def addLeaf(nodeList, pid, nodeId, resList) when nodeList == [] do
-    [resList | {pid, nodeId}]
+     resList ++ [{pid, nodeId}]
   end
 
   def addLeaf(nodeList, pid, nodeId, resList) do
     [head | tail] = nodeList
     {_, tmpId} = head
-    if (getDistance(tmpId, nodeId) > getDistance(nodeId, tmpId)) do
-      [resList | [{pid, nodeId}] | nodeList]
+    if (tmpId  > nodeId) do
+      resList ++ [{pid, nodeId}] ++ nodeList
     else
-      addLeaf(tail, pid, nodeId, [resList | head]) 
+      addLeaf(tail, pid, nodeId, resList ++ [head]) 
     end
   end
 
-  def removeLastElement(nodeList) do
-    [head | tail] = Enum.reverse(nodeList)
-    {Enum.reverse(tail), head}
-  end
-
   def addLeafNode(left, right, newPid, newNodeId, curNodeId) do
-    if (getDistance(curNodeId, newNodeId) > getDistance(newNodeId, curNodeId)) do
+    if (curNodeId > newNodeId) do
       left = addLeaf(left, newPid, newNodeId, [])
       {left, right} = if (length(left) > 8 ) do
-                        [head | tail] = left
-                        {tmpPid, tmpNodeId} = head
-                        tmpRight = addLeaf(right, tmpPid, tmpNodeId, [])
-                        {tmpRight, _ } = if (length(tmpRight) > 8) do
-                                            removeLastElement(tmpRight)
-                                         else
-                                            {tmpRight, :null}
-                                         end
-                        {tail, tmpRight}
+                        [_ | tail] = left
+                        {tail, right}
                       else
                         {left, right}  
                       end
-      {left, right}
+      {Enum.uniq(left), right}
     else
       right = addLeaf(right, newPid, newNodeId, [])
       {left, right} = if (length(right) <= 8) do
                         {left, right}
                       else
-                        {tmpRight, head} = removeLastElement(right)
-                        {tmpPid, tmpNodeId} = head
-                        tmpLeft = addLeaf(left, tmpPid, tmpNodeId, [])
-                        if (length(tmpLeft) > 8)
-                          {tl(tmpLeft), tmpRight}
-                        else
-                          {tmpLeft, tmpRight}
-                        end
+                        [_ | tail] = Enum.reverse(right)
+                        tail = Enum.reverse(tail)
+                        {left, tail}
                       end
-      {left, right}
+      {left, Enum.uniq(right)}
     end
   end
 
   def receiveLoop(routeTable, leftLeafs, rightLeafs, curNodeId, master) do
     receive do
       {:newNode, startIndex, key, pid} ->
-        startCount = startIndex
+        #IO.puts "new node " <> curNodeId <> " " <> key
         endCount = compareIds(key, curNodeId, startIndex)
         send(pid, {:routeTable, routeTable, self(), curNodeId})
         nextHop = checkInRange(leftLeafs, [{self(), curNodeId} | rightLeafs], key)
+        nextHop = if (nextHop == :null && curNodeId == key) do
+                    self()
+                  else
+                    :null
+                  end
+
         nextHop = if (nextHop == :null) do
-                    Enum.at(routeTable, endCount).at(String.to_integer( String.at(key, endCount), 16 ))
+                    tmpVal = String.to_integer( String.at(key, endCount), 16 )
+                    Enum.at(Enum.at(routeTable, endCount), tmpVal)
                   else
                     nextHop
                   end
         nextHop = if (nextHop == :null) do
-                    getNearestFromLeafs(key, [leftLeafs | rightLeafs | {self(), curNodeId}])
+                    getNearestFromLeafs(leftLeafs ++ rightLeafs ++ [{self(), curNodeId}], 
+                      String.to_integer(key, 16))
                   else
                     nextHop
                   end
@@ -209,49 +190,88 @@ defmodule Actor do
         else
           send(nextHop, {:newNode, endCount, key, pid})
         end
+        receiveLoop(routeTable, leftLeafs, rightLeafs, curNodeId, master)
       {:routeTable, sourceRouteTable, senderPid, senderNodeId} ->
         routeTable = 
-          updateRouteTable(routeTable, sourceRouteTable, to_string(curNodeId), to_string(senderNodeId), 0)
+          updateRouteTable(routeTable, sourceRouteTable, curNodeId, senderNodeId, 0)
         {leftLeafs, rightLeafs} = addLeafNode(leftLeafs, rightLeafs, senderPid, senderNodeId, curNodeId)
+        #IO.puts "route " <> curNodeId <> " " <> senderNodeId
+        #IO.inspect(leftLeafs)
+        #IO.inspect(rightLeafs)
+        #IO.inspect(routeTable)
+        receiveLoop(routeTable, leftLeafs, rightLeafs, curNodeId, master)
     end
   end
 
-  def getNullList(len, list) when len == 0 do
-    list
+  def getNullList(len, list, _) when len == 0 do
+    Enum.reverse(list)
   end
 
-  def getNullList(len, list) do
-    getNullList(len-1, [:null | list])
+  def getNullList(len, list, tar) do
+    #IO.puts tar
+    if (17 - len == tar) do
+      getNullList(len-1, [self() | list], tar)
+    else
+      getNullList(len-1, [:null | list], tar)
+    end
   end
 
-  def initRouteTable(routeTable, len)  when len == 0 do
+  def initRouteTable(routeTable, len, nodeId)  when len == 0 do
     routeTable
   end
 
-  def initRouteTable(routeTable, len) do
-    routeTable = [ getNullList(17) | routeTable]
-    initRouteTable(routeTable, len-1)
+  def initRouteTable(routeTable, len, nodeId) do
+    routeTable = 
+      [ getNullList(17, [], String.to_integer( String.at(nodeId, 32-len), 16 )) | routeTable]
+    
+    initRouteTable(routeTable, len-1, nodeId)
+    
   end
 
-  def sendRouteTable(routeTable, sendList, nodeId) when sendList == [] do
+  def sendRouteTableToLeafs(sendList, res) when sendList == [] do
+    res
+  end
+
+  def sendRouteTableToLeafs(sendList, res) do
+  #IO.puts length(sendList)
+    [head | tail] = sendList
+    {pid, _} = head
+    sendRouteTableToLeafs(tail, [pid | res])
+  end
+
+  def sendRouteTable(_, sendList, _) when sendList == [] do
     
   end
 
   def sendRouteTable(routeTable, sendList, nodeId) do
-    [head | tail] = sendList
-    {pid, _} = head
+    [pid | tail] = sendList
     send(pid, {:routeTable, routeTable, self(), nodeId})
+    #:timer.sleep(200)
     sendRouteTable(routeTable, tail, nodeId)
   end
 
   def start(startNode, nodeNum, master) do
     nodeId = Base.encode16(:crypto.hash(:sha256, to_string(nodeNum)))
     nodeId = String.slice(nodeId, 2..33)
-    if (startNode != []) do
-      send(hd(startNode), {:newNode, 0, nodeId, self()})
-    end
-    {routeTable, leftLeafs, rightLeafs, receivedFrom} = receiveRouteInfo(initRouteTable([], 33), [], [], nodeId, [])
-    sendRouteTable(routeTable, [ receivedFrom | leafNodes | rightLeafs ], nodeId)
+    #IO.puts nodeId
+    #IO.inspect (self())
+    routeTable = initRouteTable([], 32, nodeId)
+    routeTable = [ getNullList(17, [], 18) | routeTable]
+    routeTable = Enum.reverse(routeTable)
+    #IO.inspect(routeTable)
+    {routeTable, leftLeafs, rightLeafs, receivedFrom} = 
+      if (startNode != []) do
+        send(hd(startNode), {:newNode, 0, nodeId, self()})
+        receiveRouteInfo(routeTable, nodeId, [])
+      else
+        {routeTable, [], [], []}
+      end
+    #IO.inspect(routeTable)
+    #IO.inspect(leftLeafs)
+    #IO.inspect(rightLeafs)
+    receivedFrom = Enum.uniq(receivedFrom ++ sendRouteTableToLeafs(leftLeafs ++ rightLeafs, []))
+    sendRouteTable(routeTable, receivedFrom, nodeId)
+    :timer.sleep(20)
     send(master, {:done})
     receiveLoop(routeTable, leftLeafs, rightLeafs, nodeId, master)  
   end
@@ -273,7 +293,7 @@ defmodule PASTRY do
 
 
   def createNodes(numNodes, nodes) do
-    pid = if (nodes == []) do
+    pid = if (nodes != []) do
             spawn(Actor, :start, [[hd(nodes)], numNodes, self()])
           else
             spawn(Actor, :start, [[], numNodes, self()])
